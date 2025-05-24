92b1ab0debc1afaeeda5ddcb9f867a02af15b0cc
Nathan Wiles <ntwiles@gmail.com>
2025-05-24 01:51:30 -0400

    Implement scope binding already exists error.

--- src/errors/runtime_error.rs
+++ src/errors/runtime_error.rs
@@ -10,7 +10,9 @@ impl Error for RuntimeError {
         todo!()
     }
 
-    fn scope_binding_already_exists(_ident: &str) -> Self {
-        todo!()
+    fn scope_binding_already_exists(ident: &str) -> Self {
+        RuntimeError {
+            message: format!("Scope binding alrady exists for identifier \"{ident}\"."),
+        }
     }
 }
